c33050702e40c53d3e4bbbe3fe6f37c53d96bab1
Methw <37112059+Methw@users.noreply.github.com>
2018-04-12 08:08:07 +0700

    Update buildnode.sh

--- buildnode.sh
+++ buildnode.sh
@@ -3,7 +3,7 @@
 apt-get install build-essential openssl libssl-dev libudev-dev gcc libudev-dev pkg-config -y
 
 #Build and install rust
-curl https://sh.rustup.rs -sSf | sh
+curl https://sh.rustup.rs -sSf | sh -s -- -y
 source $HOME/.cargo/env
 
 
@@ -15,5 +15,4 @@ sudo cp ~/rust-callisto/target/release/parity /usr/local/bin/
 
 echo "parity --chain=galilei --no-ui --no-ws --no-dapps --jsonrpc-apis safe,web3 --tracing=on --fat-db=on --pruning=archive --public-node  --jsonrpc-cors '*' --jsonrpc-hosts all --jsonrpc-interface all" > ~/startparity.sh
 chmod +x ~/startparity.sh
-
-
+echo "To run Parity you can change file startparity.sh"
