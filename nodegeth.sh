4b77959a99e0ef35ba43c0971242db96cf76aa9b
Methw <37112059+Methw@users.noreply.github.com>
2018-04-12 08:52:43 +0700

    Create nodegeth.sh

---
+++ nodegeth.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+sudo apt-get update
+sudo apt-get -y upgrade
+sudo apt-get install make gcc musl-dev git build-essential
+sudo curl -O https://storage.googleapis.com/golang/go1.10.1.linux-amd64.tar.gz
+sudo tar -xvf go1.10.1.linux-amd64.tar.gz
+sudo mv go /usr/local
+echo "export PATH=$PATH:/usr/local/go/bin" >> ~/.profile
+
+source ~/.profile
+go version
+
+git clone -b CLO/1.0 https://github.com/EthereumCommonwealth/go-callisto.git
+cd go-callisto && make geth
+
+cp ~/go-callisto/build/bin/* /usr/local/bin/
+
+echo "Run just type : geth"
